e0311cf3c6d29601aecfc9369264dc1f4ffa7ff5
SanBast <marcianovincenzomv@gmail.com>
2023-08-23 17:54:34 +0200

    refactor code. added LCD interaction

--- sketch_start_lap_receiver/sketch_start_lap_receiver.ino
+++ sketch_start_lap_receiver/sketch_start_lap_receiver.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <LiquidCrystal.h>
 
 #define CE_PIN   9
 #define CSN_PIN 10
@@ -10,9 +11,12 @@ int greenLedPin = 15;
 int buzzerPin = 17;
 int photoresistorPin = A0;
 
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 int laserLightThreshold = 750;
 
-// BUTTON 1 WITH DEBOUNCING //
+// BUTTON 1-2 WITH DEBOUNCING //
 int button1Pin = 18;
 int button1State = 0;
 int lastButton1State = LOW;  // the previous reading from the input pin
@@ -25,6 +29,7 @@ int lastButton2State = LOW;
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
+bool firstStart = true;
 bool isCalibrated = false;
 int detectedLight = 0;
 int calibrating = false;
@@ -35,7 +40,7 @@ bool goneThrough = false;
 const uint64_t pipe = 0xE8E8F0F0E1LL; // Defines the communication channel
 RF24 radio(CE_PIN, CSN_PIN); // Sets up the communication
 int datatoreceive[4];
-
+float lastComputedLap = -1;
 
 void setup() {
   // put your setup code here, to run once:
@@ -48,6 +53,8 @@ void setup() {
   radio.openReadingPipe(1,pipe);
   radio.startListening();
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  //lcd.print("Timer Lap!");
 }
 
 void loop() {
@@ -66,21 +73,30 @@ void loop() {
   switch (systemStatus) { 	
     case 0: 
       // CASE 0 : IDLE ( Start lap not pressed or computation terminated / no calibration in progress or calibration just terminated)
-      Serial.println("IDLE...");  
+      if(isCalibrated == true && firstStart == false){
+      lcdPrintFirstLine("IDLE...CALIBRATION OK"); 
+      } else if(isCalibrated == false && firstStart == true){
+        lcdPrintFirstLine("NEED CALIBRATION ");
+      } else if(isCalibrated == false && firstStart == false){
+        lcdPrintFirstLine("LOST CALIBRATION !");
+      }
       checkCalibration(detectedLight);
+      if(lastComputedLap != -1){
+        Serial.println(lastComputedLap);
+        lcdPrintSecondLine(lastComputedLap);
+      }
 			break;
 
 		case 1: 
       // CASE 1 : CALIBRATION IN PROGRESS
-      Serial.println("CALIBRATION IN PROGRESS");
+      lcdPrintFirstLine("CALIBRATION IN PROGRESS");
       calibrateLaser(detectedLight);
 			break; 
 
 		case 2: 
       // CASE 2 : LAP CALCULATION IN PROGRESS
-			//Serial.println("LAP CALCULATION IN PROGRESS");
+			lcdPrintFirstLine("LAP CALCULATION IN PROGRESS");
       startLapMeasurement(detectedLight);
-      radioComm();
 			break; 
 		}
   }
@@ -147,6 +163,7 @@ void calibrateLaser(int lightLevel){
       digitalWrite(buzzerPin, LOW);
       systemStatus = 0;
       isCalibrated = true;
+      firstStart = false;
     }
  }
  else {
@@ -162,7 +179,8 @@ void calibrateLaser(int lightLevel){
 void checkCalibration(int lightLevel){
   long start = millis();
   if(lightLevel < laserLightThreshold && isCalibrated == true) {
-    Serial.println("LOST CALIBRATION");
+    lcd.clear();
+    lcdPrintFirstLine("LOST CALIBRATION");
     digitalWrite(greenLedPin, LOW);
     digitalWrite(redLedPin, HIGH);
     isCalibrated = false;
@@ -181,15 +199,18 @@ void checkCalibration(int lightLevel){
 
 long start = 0;
 void startLapMeasurement(int lightLevel){
-  digitalWrite(greenLedPin, LOW);
-  delay(500);
+  blinkingLedWithoutDelay(greenLedPin);
   if (lightLevel < laserLightThreshold && goneThrough == false){
     goneThrough = true;
-    digitalWrite(greenLedPin, HIGH);
     start = millis();
-  } 
-  Serial.println(millis() - start);
+  }
+  if (goneThrough)
+  { 
+    lcdPrintSecondLine(float(millis() - start)/float(1000));
+    radioComm();
 }
+}
+
 
 
 void radioComm(){
@@ -208,18 +229,22 @@ void radioComm(){
  */
 
   if ( radio.available() ){
-    Serial.println("RADIO AVAILABLE");
+    //Serial.println("RADIO AVAILABLE");
     // leggi i dati in ricezione finchè il messaggio è completo
     bool done = false;
     while (!done){
       // ricevi il messaggio
       radio.read( datatoreceive, sizeof(datatoreceive) );
       if(datatoreceive[0] == 1 && datatoreceive[1] == 1 && datatoreceive[2] == 9 && datatoreceive[3] == 5){
+        done = true;
         digitalWrite(buzzerPin, HIGH);
         delay(200);
         digitalWrite(buzzerPin, LOW);
         delay(200);
-        Serial.println("DATA TO RECEIVE :");
+        goneThrough = false;
+        systemStatus = 0;
+        lastComputedLap =  float(millis() - start)/float(1000);
+        /*Serial.println("DATA TO RECEIVE :");
         Serial.print(datatoreceive[0]);
         Serial.print(" ");
         Serial.print(datatoreceive[1]);
@@ -227,8 +252,58 @@ void radioComm(){
         Serial.print(datatoreceive[2]);
         Serial.print(" ");
         Serial.print(datatoreceive[3]);
-        Serial.println(" ");
+        Serial.println(" ");*/
       }
     }
   }
+}
+
+// VARIABLES BLINKING LED //
+int ledState = LOW;  // ledState used to set the LED
+unsigned long previousMillis = 0;  // will store last time LED was updated
+const long interval = 500;  // interval at which to blink (milliseconds)
+
+void blinkingLedWithoutDelay(int ledPin){
+    unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
+}
+
+void lcdPrintFirstLine(char* wordPrint){
+  lcd.setCursor(0, 0);
+  lcd.print(scrollFirstLine(wordPrint));
+  delay(200);
+}
+
+void lcdPrintSecondLine(float value){
+  lcd.setCursor(0, 1);
+  lcd.print(value);
+}
+
+int i = 16;
+int ii = 0;
+String scrollFirstLine(String wordDisplay){
+  String result;
+  String processStr = "                " + wordDisplay + "                ";
+  result = processStr.substring(i, ii);
+  i++;
+  ii++;
+  if (i > processStr.length()){
+    i = 16;
+    ii = 0;
+  }
+  return result;
 }
