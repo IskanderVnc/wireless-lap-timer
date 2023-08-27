393beb0fc1f0fa57db73c222a3a388c505e5bcfe
IskanderVnc <alessandro.vnc98@gmail.com>
2023-08-27 12:00:24 +0200

    Solved a critical bug causing the timer to restart in background when : 1) removing light and leave the the device without it until also 2) cancelling the lap computation; --> in the successive lap computation the lap computation would still be going ( STABLE VERSION )

--- sketch_start_lap_receiver/sketch_start_lap_receiver.ino
+++ sketch_start_lap_receiver/sketch_start_lap_receiver.ino
@@ -61,15 +61,15 @@ void loop() {
   // put your main code here, to run repeatedly:
   detectedLight = analogRead(photoresistorPin);
   //Serial.println(detectedLight);
-  if (isCalibrated){
+  if (isCalibrated && systemStatus != 2){
     readButtonWithDebouncing(button2Pin,button2State,lastButton2State);
     digitalWrite(redLedPin, LOW);  
-  }
-  else{
+  } else {
     digitalWrite(redLedPin, HIGH);
     readButtonWithDebouncing(button1Pin,button1State,lastButton1State);
   }
-  
+  Serial.print("SystemSTATUS :");
+  Serial.println(systemStatus);
   switch (systemStatus) { 	
     case 0: 
       // CASE 0 : IDLE ( Start lap not pressed or computation terminated / no calibration in progress or calibration just terminated)
@@ -81,24 +81,25 @@ void loop() {
         lcdPrintFirstLine("CALIBRATE LASER TO START ");
         digitalWrite(redLedPin,HIGH);
       } else if(isCalibrated == false && firstStart == false){
-        lcdPrintFirstLine("CALIBRATION LOST !");
+        lcdPrintFirstLineNoScrolling("CALIBRATION LOST !");
       }
+      Serial.println("IDLE");
       checkCalibration(detectedLight);
       if(lastComputedLap != -1){
-        Serial.println(lastComputedLap);
+        //Serial.println(lastComputedLap);
         lcdPrintSecondLine(lastComputedLap);
       }
 			break;
 
 		case 1: 
       // CASE 1 : CALIBRATION IN PROGRESS
-      lcdPrintFirstLine("CALIBRATION IN PROGRESS");
+      lcdPrintFirstLineNoScrolling("CALIBRATION...");
       calibrateLaser(detectedLight);
 			break; 
 
 		case 2: 
       // CASE 2 : LAP CALCULATION IN PROGRESS
-      Serial.println("LAP CALCULATION");
+      //Serial.println("LAP CALCULATION");
 			lcdPrintFirstLine("LAP CALCULATION IN PROGRESS");
       readButtonWithDebouncing(button2Pin,button2State,lastButton2State);
       startLapMeasurement(detectedLight);
@@ -130,21 +131,23 @@ void readButtonWithDebouncing(int buttonPin, int &buttonState, int &lastButtonSt
       // only toggle the LED if the new button state is HIGH
       if (buttonState == HIGH) {
         if(buttonPin == 19){
+          if(systemStatus != 2){
+            systemStatus = 2;
+            goneThrough = false;
+          } else {
+            //checkCalibration(detectedLight);
+            Serial.println("SETTING SYS STATUS TO 0 AFTER BUTTON PRESSURE");
+            systemStatus = 0;
+           // goneThrough = false;
+          }
+        lcd.clear();
+        } else  {
+          systemStatus = !systemStatus;
         digitalWrite(buzzerPin, HIGH);
         delay(150);
         digitalWrite(buzzerPin, LOW);
         lcd.clear();
-          if(systemStatus != 2){
-            systemStatus = 2;
-          } else {
-            goneThrough = false;
-            systemStatus = 0;
-          }
-          
-        } 
-          
-        else 
-          systemStatus = !systemStatus;
+        }
       }
     }
   }
@@ -197,7 +200,8 @@ void checkCalibration(int lightLevel){
   long start = millis();
   if(lightLevel < laserLightThreshold && isCalibrated == true) {
     lcd.clear();
-    lcdPrintFirstLine("LOST CALIBRATION");
+    Serial.println("CALIBRATION LOST!!!");
+    lcdPrintFirstLineNoScrolling("LOST CALIBRATION");
     digitalWrite(greenLedPin, LOW);
     digitalWrite(redLedPin, HIGH);
     isCalibrated = false;
@@ -215,38 +219,27 @@ void checkCalibration(int lightLevel){
 
 
 long start = 0;
+int buzzerCounter = 0;
 void startLapMeasurement(int lightLevel){
   blinkingLedWithoutDelay(greenLedPin);
   if (lightLevel < laserLightThreshold && goneThrough == false){
+    Serial.println("LAP STARTED AND GONE THROUGH SET TO TRUE");
+    buzzerOnOff(buzzerPin);
     goneThrough = true;
     start = millis();
   }
   if (goneThrough)
   { 
+    Serial.println("GONE THROUGH");
+    buzzerOnOff(buzzerPin);
     lcdPrintSecondLine(float(millis() - start)/float(1000));
     radioComm();
 }
 }
 
-
-
 void radioComm(){
-  /* Basato sull'esempio "YourDuinoStarter Example: nRF24L01 Transmit Joystick values"
- http://arduino-info.wikispaces.com/Nrf24L01-2.4GHz-HowTo
- http://www.danielealberti.it/2016/01/ricetrasmettitori-rf-per-arduino.html
- Collegamenti del modulo RF24L01 ad Arduino:
- 1 - GND
- 2 - VCC 3.3V !!! NOT 5V
- 3 - CE to Arduino pin 9
- 4 - CSN to Arduino pin 10
- 5 - SCK to Arduino pin 13
- 6 - MOSI to Arduino pin 11
- 7 - MISO to Arduino pin 12
- 8 - UNUSED
- */
-
 // Add "&& (millis()-start > 5000" in the if - if a minimum threshold is needed
-  if ( radio.available() )){
+  if ( radio.available() ){
     //Serial.println("RADIO AVAILABLE");
     // leggi i dati in ricezione finchè il messaggio è completo
     bool done = false;
@@ -258,6 +251,7 @@ void radioComm(){
         goneThrough = false;
         systemStatus = 0;
         done = true;
+        buzzerCounter = 0;
         digitalWrite(buzzerPin, HIGH);
         delay(200);
         digitalWrite(buzzerPin, LOW);
@@ -274,6 +268,34 @@ void radioComm(){
         Serial.println(" ");*/
       }
     }
+  }
+}
+
+// VARIABLES BUZZER ON/OFF //
+int buzzerState = LOW;
+unsigned long previousMillisBuzzer = 0;  // will store last time Buzzer state was updated
+const long intervalBuzzer = 200;  // interval at which to on/off buzzer (milliseconds)
+
+void buzzerOnOff(int buzzerPin){
+    unsigned long currentMillis = millis();
+  if (currentMillis - previousMillisBuzzer >= intervalBuzzer && buzzerCounter < 9) {
+        buzzerCounter++;
+    // save the last time you enabled the buzzer
+    previousMillisBuzzer = currentMillis;
+
+    // if the buzzer is off turn it on and vice-versa:
+    if (buzzerState == LOW) {
+      Serial.println("SETTING BUZZER TO HIGH");
+      buzzerState = HIGH;
+    } else {
+      Serial.println("SETTING BUZZER TO LOW");
+      buzzerState = LOW;
+    }
+
+    // set the buzzer with the buzzerState of the variable:
+    digitalWrite(buzzerPin, buzzerState);
+  } else {
+    digitalWrite(buzzerPin,LOW);
   }
 }
 
@@ -304,7 +326,13 @@ void blinkingLedWithoutDelay(int ledPin){
 void lcdPrintFirstLine(char* wordPrint){
   lcd.setCursor(0, 0);
   lcd.print(scrollFirstLine(wordPrint));
-  delay(200);
+  delay(150);
+}
+
+void lcdPrintFirstLineNoScrolling(char* wordPrint){
+  lcd.setCursor(0, 0);
+  lcd.print(wordPrint);
+  delay(150);
 }
 
 void lcdPrintSecondLine(float value){
